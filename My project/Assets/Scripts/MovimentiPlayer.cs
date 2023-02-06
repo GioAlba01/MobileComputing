84f3e1a4eb10f611dacfa94df8017c59c0ff7487
GioAlba01 <giovannialbanese100401@gmail.com>
2023-02-06 16:01:45 +0100

    Commit 3

--- My project/Assets/Scripts/MovimentiPlayer.cs
+++ My project/Assets/Scripts/MovimentiPlayer.cs
@@ -13,7 +13,16 @@ public class MovimentiPlayer : MonoBehaviour
 
     private void Update()
     {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed,body.velocity.y);
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        body.velocity = new Vector2(horizontalInput * speed,body.velocity.y);
+
+        // Cambio del player quando si muove a destra o sinistra
+        if (horizontalInput > 0.01f)
+            transform.localScale = Vector3.one;
+
+        else if (horizontalInput < -0.01f)
+               transform.localScale = new Vector3(-1, 1, 1);
 
         if (Input.GetKey(KeyCode.Space))
             body.velocity = new Vector2(body.velocity.x, speed);
