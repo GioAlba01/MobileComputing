a2e65eca8b70f119736c5e241ab9e2f890eb3299
GioAlba01 <giovannialbanese100401@gmail.com>
2023-02-09 15:12:32 +0100

    Commit 11
    Saw

--- My project/Assets/Scripts/Health/Health.cs
+++ My project/Assets/Scripts/Health/Health.cs
@@ -5,10 +5,13 @@ public class Health : MonoBehaviour
     [SerializeField] private float startingHealth;
 
     public float currentHealth { get; private set; }
+    private Animator anim;
+    private bool dead;
 
     private void Awake()
     {
         currentHealth = startingHealth;
+        anim = GetComponent<Animator>();
     }
 
     public void TakeDamage(float _damage)
@@ -17,17 +20,17 @@ public class Health : MonoBehaviour
 
         if (currentHealth > 0)
         {
-            //hurt
+            anim.SetTrigger("hurt");
+            //iframes
         }
         else
         {
-            //dead
+            if (!dead)
+            {
+                anim.SetTrigger("die");
+                GetComponent<MovimentiPlayer>().enabled = false;
+                dead = true;
+            }
         }
-    }
-
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.E))
-            TakeDamage(1);
     }
 }
