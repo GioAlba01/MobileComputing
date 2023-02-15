2b7e0f8bbb66d3b35db494c992bc83091fe2503b
GioAlba01 <giovannialbanese100401@gmail.com>
2023-02-16 00:16:53 +0100

    Commit 26
    Main Menu quasi completo

--- My project/Assets/Scripts/UI/MenuManager.cs
+++ My project/Assets/Scripts/UI/MenuManager.cs
@@ -42,18 +42,19 @@ public class MenuManager : MonoBehaviour
     {
         arrow.position = new Vector3(arrow.position.x, buttons[currentPosition].position.y);
     }
-    private void Interact()
+    public void Interact()
     {
         SoundManager.instance.PlaySound(interactSound);
 
         if (currentPosition == 0)
         {
             //Start game
-            SceneManager.LoadScene(PlayerPrefs.GetInt("level", 1));
+            SceneManager.LoadScene(PlayerPrefs.GetInt("Level1", 1));
         }
         else if (currentPosition == 1)
         {
             //Open Settings
+            SceneManager.LoadScene(PlayerPrefs.GetInt("Settings", 3));
         }
         else if (currentPosition == 2)
         {
@@ -62,4 +63,19 @@ public class MenuManager : MonoBehaviour
         else if (currentPosition == 3)
             Application.Quit();
     }
+
+    #region Settings
+    public void SoundVolume()
+    {
+        SoundManager.instance.ChangeSoundVolume(0.2f);
+    }
+    public void MusicVolume()
+    {
+        SoundManager.instance.ChangeMusicVolume(0.2f);
+    }
+    public void BackMenu()
+    {
+        SceneManager.LoadScene(PlayerPrefs.GetInt("_MainMenu", 0));
+    }
+    #endregion
 }
