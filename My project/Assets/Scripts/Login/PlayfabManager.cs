f53dfa4e66b2d67ad45c320a339933133f396a37
GioAlba01 <giovannialbanese100401@gmail.com>
2023-02-18 21:04:04 +0100

    Commit 31
    Playfab

---
+++ My project/Assets/Scripts/Login/PlayfabManager.cs
@@ -0,0 +1,105 @@
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine.UI;
+
+public class PlayfabManager : MonoBehaviour
+{
+    [Header("UI")]
+    public Text messageText;
+    public InputField emailInput;
+    public InputField passwordInput;
+
+    public void RegisterButton()
+    {
+        var request = new RegisterPlayFabUserRequest
+        {
+            Email = emailInput.text,
+            Password = passwordInput.text,
+            RequireBothUsernameAndEmail = false
+        };
+        PlayFabClientAPI.RegisterPlayFabUser(request, OnRegisterSuccess, OnError);
+    }
+
+    void OnRegisterSuccess(RegisterPlayFabUserResult result)
+    {
+        messageText.text = "Registered and logged in!";
+    }
+
+    public void LoginButton()
+    {
+
+    }
+
+    public void ResetPasswordButton()
+    {
+
+    }
+
+    void Start()
+    {
+        Login();
+    }
+
+    void Login()
+    {
+        var request = new LoginWithCustomIDRequest
+        {
+            CustomId = SystemInfo.deviceUniqueIdentifier,
+            CreateAccount = true
+        };
+        PlayFabClientAPI.LoginWithCustomID(request, OnSuccess, OnError);
+    }
+
+    void OnSuccess(LoginResult result)
+    {
+        Debug.Log("Successful login/account create!");
+    }
+
+    void OnError(PlayFabError error)
+    {
+        Debug.Log("Error while logging in/creating account!");
+        Debug.Log(error.GenerateErrorReport());
+    }
+
+    public void SendLeaderboard(int score)
+    {
+        var request = new UpdatePlayerStatisticsRequest
+        {
+            Statistics = new List<StatisticUpdate>
+            {
+                new StatisticUpdate
+                {
+                    StatisticName = "PlatformScore",
+                    Value = score
+                }
+            }
+        };
+        PlayFabClientAPI.UpdatePlayerStatistics(request, OnLeaderboardUpdate, OnError);
+    }
+
+    void OnLeaderboardUpdate (UpdatePlayerStatisticsResult result)
+    {
+        Debug.Log("Successfull leaderboard sent");
+    }
+
+    public void GetLeaderboard()
+    {
+        var request = new GetLeaderboardRequest
+        {
+            StatisticName = "PlatformScore",
+            StartPosition = 0,
+            MaxResultsCount = 10
+        };
+        PlayFabClientAPI.GetLeaderboard(request, OnLeaderboardGet, OnError);
+    }
+
+    void OnLeaderboardGet (GetLeaderboardResult result)
+    {
+        foreach (var item in result.Leaderboard)
+        { 
+            Debug.Log(item.Position + " " + item.PlayFabId + " " + item.StatValue);
+        }
+    }
+}
